2bd33381a2340a097912544d76bf1c604d9a5668
heku777 <chotips08@gmail.com>
2021-07-14 10:06:02 +0900

    #9 kivy ios tutorial

--- kivy/my_firebase.py
+++ kivy/my_firebase.py
@@ -1,9 +1,71 @@
-class MyFirebase:
-    def sign_up(self, email, password):
-        # Send email and password to firebase
+import requests
+import json
 
-        # Firebase will return localID, authToken, refreshToken
-        pass
+from kivy.app import App
+
+
+class MyFirebase:
+    # Web API key
+    wak = "AIzaSyDtDBKDRmjoYmHaTqosiadbsRc91cuX9kk"
+
+    def sign_up(self, email, password):
+        app = App.get_running_app()
+        # Send email and password to firebase
+        # Firebase will return localID, authToken(idToken), refreshToken
+        signup_url = f"https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key={self.wak}"
+        signup_payload = {"email": email, "password": password, "returnSecureToken": True}
+        sign_up_request = requests.post(signup_url, data=signup_payload)
+        # print(sign_up_request.ok)
+        # print(sign_up_request.content.decode())
+        sign_up_data = json.loads(sign_up_request.content.decode())
+
+        if sign_up_request.ok:
+            refresh_token = sign_up_data["refreshToken"]
+            localId = sign_up_data["localId"]
+            idToken = sign_up_data["idToken"]
+            # Save refresh Token to a file
+            with open("refresh_token.txt", "w") as f:
+                f.write(refresh_token)
+            # Save localId to a variable in main app class
+            # Save idToken to a variable in main app class
+            app.localId = localId
+            app.id_token = idToken
+
+            # Create new key in database from localId
+            # Get friend ID
+            # Get request on firebase to get the next friend id
+            # Update firebase's next friend id field
+            friend_get_req = requests.get(
+                f"https://tutorial-78ce1-default-rtdb.firebaseio.com/next_friend_id.json?auth={idToken}"
+            )
+            my_friend_id = friend_get_req.json()
+
+            print(friend_get_req.json())
+            # Default streak
+            # Default Avatar
+            # Friends list
+            # Empty workouts area
+            my_data = '{"avatar": "man.png", "friends": "", "workouts": "", "streak ": "0", "my_friend_id": %s}' % \
+                      my_friend_id
+            post_request = requests.patch(
+                f"https://tutorial-78ce1-default-rtdb.firebaseio.com/{localId}.json?auth={idToken}",
+                data=my_data)
+            # print(post_request.ok)
+            # print(json.loads(post_request.content.decode()))
+            app.change_screen("home_screen")
+
+        if not sign_up_request.ok:
+            error_data = json.loads(sign_up_request.content.decode())
+            error_message = error_data["error"]["message"]
+            app.root.ids["login_screen"].ids["login_message"].text = error_message
+
+    def exchange_refresh_token(self, refresh_token):
+        refresh_url = "https://securetoken.googleapis.com/v1/token?key=" + self.wak
+        refresh_payload = '{"grant_type": "refresh_token", "refresh_token": "%s"}' % refresh_token
+        refresh_req = requests.post(refresh_url, data=refresh_payload)
+        id_token = refresh_req.json()['id_token']
+        local_id = refresh_req.json()['user_id']
+        return id_token, local_id
 
     def sign_in(self):
         pass
--- kivy/tutorial.py
+++ kivy/tutorial.py
@@ -1,7 +1,7 @@
 import kivy.uix.label
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import Screen, NoTransition, CardTransition
 from kivy.uix.button import ButtonBehavior
 from kivy.uix.image import Image
 from kivy.uix.label import Label
@@ -45,22 +45,14 @@ GUI = Builder.load_file("tutorial.kv")
 class Tutorial(App):
     my_friend_id = 1
 
-    def build(self):
+    def __init__(self, **kwargs):
+        super().__init__()
         self.my_firebase = MyFirebase()
+
+    def build(self):
         return GUI
 
     def on_start(self):
-
-        # Get database data
-        result = requests.get(f"https://tutorial-78ce1-default-rtdb.firebaseio.com/{str(self.my_friend_id)}.json")
-        # print("Was it okay?", result.ok)
-        data = json.loads(result.content.decode())
-        # print(data)
-
-        # Get and updates avatar image
-        avatar_image = self.root.ids["avatar_image"]
-        avatar_image.source = "icons/avatars/" + data["avatar"]
-
         # Populate avatar grid
         avatar_grid = self.root.ids["change_avatar_screen"].ids["avatar_grid"]
         for root_dir, folders, files in walk("icons/avatars"):
@@ -68,25 +60,51 @@ class Tutorial(App):
                 img = ImageButton(source=f"icons/avatars/{f}", on_release=partial(self.change_avatar, f))
                 avatar_grid.add_widget(img)
 
-        # Get and updates streak label
-        streak_label = self.root.ids["home_screen"].ids["streak_label"]
-        streak_label.text = str(data["streak"]) + "Day Streak!"
+        try:
+            # Try to read the persistem signin credentials (refresh token)
+            with open("refresh_token.txt", "r") as f:
+                refresh_token = f.read()
 
-        # Get and update friend id label
-        friend_id_label = self.root.ids["settings_screen"].ids["friend_id_label"]
-        friend_id_label.text = f"Friend ID: {str(self.my_friend_id)}"
+            # Use refresh token to get a new idToken
+            id_token, local_id = self.my_firebase.exchange_refresh_token(refresh_token)
 
-        banner_grid = self.root.ids["home_screen"].ids["banner_grid"]
-        workouts = data["workouts"][1:]
-        for workout in workouts:
-            for _ in range(5):
-                # Populate workout grid in home screen
-                W = WorkoutBanner(workout_image=workout["workout_image"], description=workout["description"],
-                                  type_image=workout["type_image"], number=workout["number"], units=workout["units"],
-                                  likes=workout["likes"])
-                # print(workout["workout_image"])
-                # print(workout["units"])
-                banner_grid.add_widget(W)
+            # Get database data
+            result = requests.get(f"https://tutorial-78ce1-default-rtdb.firebaseio.com/{local_id}.json?auth={id_token}")
+            # print("Was it okay?", result.ok)
+            # print(result)
+            data = json.loads(result.content.decode())
+            # print(f"data = {data}")
+
+            # Get and updates avatar image
+            avatar_image = self.root.ids["avatar_image"]
+            avatar_image.source = "icons/avatars/" + data["avatar"]
+
+            # Get and updates streak label
+            streak_label = self.root.ids["home_screen"].ids["streak_label"]
+            streak_label.text = str(data["streak"]) + "Day Streak!"
+
+            # Get and update friend id label
+            friend_id_label = self.root.ids["settings_screen"].ids["friend_id_label"]
+            friend_id_label.text = f"Friend ID: {str(self.my_friend_id)}"
+
+            banner_grid = self.root.ids["home_screen"].ids["banner_grid"]
+            workouts = data["workouts"][1:]
+            for workout in workouts:
+                for _ in range(5):
+                    # Populate workout grid in home screen
+                    W = WorkoutBanner(workout_image=workout["workout_image"], description=workout["description"],
+                                      type_image=workout["type_image"], number=workout["number"], units=workout["units"],
+                                      likes=workout["likes"])
+                    # print(workout["workout_image"])
+                    # print(workout["units"])
+                    banner_grid.add_widget(W)
+
+            self.root.ids["screen_manager"].transition = NoTransition()
+            self.change_screen("home_screen")
+            self.root.ids["screen_manager"].transition = CardTransition()
+
+        except Exception as e:
+            print(e)
 
     def change_avatar(self, image, widget_id):
         # Change avatar in the app
@@ -105,7 +123,6 @@ class Tutorial(App):
         print(self.root.ids)
         screen_manager = self.root.ids["screen_manager"]
         screen_manager.current = screen_name
-
 
 
 if __name__ == '__main__':
